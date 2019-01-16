a7d43d65e364a514de178728c948fff3440e5889
ADARSH N BIDARI <40069688+adarshnbidari@users.noreply.github.com>
2019-01-17 00:57:30 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express=require('express');
 const hbs=require('hbs'); 
 
 const fs=require('fs'); 
+
+const port=process.env.PORT || 3000;
  
 var app=express();
 
@@ -60,6 +62,6 @@ app.get('/bad',(req,res)=>{
 	});
 });
 
-app.listen(3000,()=>{
-	console.log("server is running in port 3000");
+app.listen(port,()=>{
+	console.log(`server is running in port ${port}`);
 });
